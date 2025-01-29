0b58bb50112ac8efa109eb5ddf7e7fd051317682
Gadhave Om Sanjay <gadhave.omibaba27@gmail.com>
2025-01-29 17:09:30 +0530

    Implement SignUp form with validation

--- frontend/notes-app/src/components/Navbar/Input/PasswordInput.jsx
+++ frontend/notes-app/src/components/Navbar/Input/PasswordInput.jsx
@@ -13,7 +13,7 @@ const PasswordInput = ({ value, onChange, placeholder }) => {
         <div className='flex items-center bg-transparent border-[1.5px] px-5 rounded mb-3'>
             <input
                 value={value}
-                onChange={onchange}
+                onChange={onChange}
                 type={isShowPassword ? "text" : "password"}
                 placeholder={placeholder || "Password"}
                 className='w-full text-sm bg-transparent py-3 mr-3 rounded outline-none'
