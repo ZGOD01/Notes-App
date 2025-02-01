80d0204307b85bfbbf819bfff33cb8c2f10ef621
Gadhave Om Sanjay <gadhave.omibaba27@gmail.com>
2025-02-01 15:44:10 +0530

    Integrated Get All Notes API

--- backend/index.js
+++ backend/index.js
@@ -207,6 +207,27 @@ app.put('/edit-note/:noteId', authenticateToken, async (req, res) => {
 })
 
 // Get All Note 
+app.get('/get-all-notes/', authenticateToken, async (req, res) => {
+    const { user } =req.user;
+
+    try {
+        const notes = await Note.find({ userId: user._id}).sort( { isPinned: -1});
+
+        return res.json({
+            error: false,
+            notes,
+            message: "Notes fetched successfully"
+        });
+
+    } catch (error) {
+        return res
+           .status(500)
+           .json({
+                error: true, message: "Internal Server Error"
+            });
+    }
+})
+
 
 
 app.listen(8000)
